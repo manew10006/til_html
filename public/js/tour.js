8a42b475b4e71c694abe58c6dd9fbb3524cdc9d7
manew10006 <100.0.6.biz@gmail.com>
2025-05-22 12:07:59 +0900

    tour 작업

--- public/js/tour.js
+++ public/js/tour.js
@@ -1,3 +1,5 @@
+// 이미지 등의 리소스가 불러들여지면 그때 슬라이드 처리 가 자연스러움.
+// load 이벤트를 썻다
 window.addEventListener("load", function () {
   // api 주소
   const apiUrl = "http://127.0.0.1:5500/public/api/tour.json";
@@ -9,6 +11,8 @@ window.addEventListener("load", function () {
   let cateNameArr = [];
   // 카테고리별 목록 배열
   let cateListArr = [];
+  // 투어 슬라이드 변수
+  let swTour;
 
   // api 호출 함수
   async function getData(_url, _fn) {
@@ -23,7 +27,7 @@ window.addEventListener("load", function () {
 
   // 데이터를 처리하는 함수
   const parseData = function (_data) {
-    console.log(_data);
+    // console.log(_data);
 
     // 1. 카테고리 글자만 추출하기
     // const cateArr = _data.map(item => item.cate );
@@ -38,14 +42,10 @@ window.addEventListener("load", function () {
     });
     cateListArr = [...listArr];
 
-    tourStar();
-  };
-
-  function tourStar() {
     makeTourInit();
     activeCateFocus();
     addBtsEvent();
-  }
+  };
 
   // 최초 데이터가 전달되면 1 번만 실행
   // 카테고리 버튼은 1 번만 만들어도 됨.
@@ -59,7 +59,6 @@ window.addEventListener("load", function () {
     //   const tag = `<li><button>${cateNameArr[i]}</button></li>`;
     //   html = html + tag;
     // }
-    // foreach로 만들 수 있다
 
     cateNameArr.forEach(function (item, index) {
       const tag = `<li><button>${item}</button></li>`;
@@ -68,7 +67,7 @@ window.addEventListener("load", function () {
 
     cateBts.innerHTML = html;
 
-    //버튼 태그 모음
+    // 버튼 태그 모음
     cateButtonArr = document.querySelectorAll(
       ".section_category_bts > li > button"
     );
@@ -78,7 +77,7 @@ window.addEventListener("load", function () {
   function addBtsEvent() {
     cateButtonArr.forEach(function (item, index) {
       item.addEventListener("click", function () {
-        //  동일한 버튼 클릭 시 작동 방지
+        // 동일한 버튼 클릭시 작동 방지
         if (cateFocusIndex === index) {
           return;
         }
@@ -90,7 +89,7 @@ window.addEventListener("load", function () {
   }
 
   // 포커스 모두 제거하기
-  function removeFocusAll(_index) {
+  function removeFocusAll() {
     cateButtonArr.forEach(function (item) {
       item.classList.remove("cate_focus");
     });
@@ -99,6 +98,66 @@ window.addEventListener("load", function () {
   // 카테고리 버튼에 현재 포커스 표현하기
   function activeCateFocus() {
     cateButtonArr[cateFocusIndex].classList.add("cate_focus");
+    makeTourListHtml();
+  }
+
+  // 목록 html 을 만든다.
+  function makeTourListHtml() {
+    console.log("어느 목록을 출력할 것인가? " + cateListArr[cateFocusIndex]);
+    // 1. html 태그만들기
+    // 1.1. 어디다가 만들지? querySelector 찾아줌.
+    const swTourWrap = document.querySelector(".sw_tour .swiper-wrapper");
+    // 1.2. html 로 만들기
+    let html = "";
+    const listArr = cateListArr[cateFocusIndex];
+    listArr.forEach(function (item) {
+      const tag = `
+      <div class="swiper-slide">
+        <div class="item">
+          <a href="${item.link}">
+            <div class="item_image">
+              <img
+                src="${item.image}"
+                alt="${item.title}"
+                title="${item.title}"
+              />
+            </div>
+            <span class="item_name">${item.city}</span>
+            <div class="item_text">
+              <span class="item_cupon">
+              ${item.title}
+              </span>
+              <p class="item_desc">
+              ${item.content}
+              </p>
+              <span class="item_price"><b>${item.price}</b>원~</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      `;
+      html = html + tag;
+    });
+    swTourWrap.innerHTML = html;
+
+    // 2. 항상 슬라이드가 만들어져 있다면 삭제하고
+    if (swTour) {
+      // swiper 를 제거합니다. (swiper 사이트의 레퍼런스 참조)
+      swTour.destroy(true, true);
+    }
+
+    // 3. 슬라이드를 생성해야 합니다.
+    swTour = new Swiper(".sw_tour", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      slidesPerGroup: 3,
+      navigation: {
+        nextEl: ".tour_next",
+        prevEl: ".tour_prev",
+      },
+    });
+
+    // 2. swiper 생성
   }
 
   // 함수 호출
